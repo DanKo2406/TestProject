1e7531af5c878d7460906268d9703cdf09c37786
DanKo2406 <DanKo2406@gmail.com>
2022-08-23 18:03:30 +0300

    CreateProject

---
+++ js.js
@@ -0,0 +1,14 @@
+let div = document.getElementById('div')
+div.onclick = () => {
+  let textarea = document.createElement('textarea')
+  textarea.value = div.innerHTML
+  textarea.onblur = () => {
+    div.innerHTML = textarea.value
+      textarea.replaceWith(div)
+  }
+  div.replaceWith(textarea)
+  textarea.focus() 
+}
+
+
+document.addEventListener('DOMContentLoaded', () => alert("Загружено!"));
